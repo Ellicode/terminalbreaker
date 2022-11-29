902dfebf795142424b27adc684a3f8f0db7d124f
Elliot <76738069+Ellicode@users.noreply.github.com>
2022-11-29 13:47:24 -0500

    forgot version check

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='terminalbreaker',
-    version='0.1.4',
+    version='0.1.5',
     license='MIT',
     author="Ellicode",
     author_email='ellicode22@gmail.com',
