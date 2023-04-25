f2c92f7bd358cc3e3d9f1ac036d1b0a2a399e562
Elliot <76738069+Ellicode@users.noreply.github.com>
2023-04-25 19:21:11 -0400

    added update script

--- build_and_publish.bat
+++ build_and_publish.bat
@@ -1,2 +1,11 @@
-py -m build
-py -m twine upload dist/*
+@echo off
+set /P confirm="[35mDo you really want to build and update terminalbreaker's version?[0m [y/n] : "
+IF %confirm% EQU "y" (
+    echo [35mSTARTING BUILD...[0m
+    py -m build
+    echo [35mSTARTING UPLOAD...[0m
+    py -m twine upload dist/*      
+    echo [35mDONE![0m
+) ELSE (
+    echo [31mCANCELED BY USER[0m
+)
